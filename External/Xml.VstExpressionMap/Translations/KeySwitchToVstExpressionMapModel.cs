94cba16cedd3dcf0d62249d513761ec431269f42
Hiroaki@R-Koubou <dev.r.koubou@gmail.com>
2020-09-19 18:45:04 +0900

    WIP: VstExpressionMap
    プロジェクトを追加

---
+++ External/Xml.VstExpressionMap/Translations/KeySwitchToVstExpressionMapModel.cs
@@ -0,0 +1,14 @@
+using KeySwitchManager.Domain.Commons;
+using KeySwitchManager.Domain.KeySwitches.Aggregate;
+using KeySwitchManager.Domain.Translations;
+
+namespace Xml.VstExpressionMap.Translations
+{
+    public class KeySwitchToVstExpressionMapModel : IKeySwitchToText
+    {
+        public IText Translate( KeySwitch source )
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
