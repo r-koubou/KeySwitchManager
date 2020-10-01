6b66e3328996405b7a6ad79977012d6850da9882
Hiroaki@R-Koubou <dev.r.koubou@gmail.com>
2020-10-02 00:02:47 +0900

    Dictionary上での検索が正しく行われるように、各Equalityメンバを実装

--- Domain/KeySwitches/Value/ExtraDataKey.cs
+++ Domain/KeySwitches/Value/ExtraDataKey.cs
@@ -15,11 +15,45 @@ namespace KeySwitchManager.Domain.KeySwitches.Value
             Value = key;
         }
 
+        public override string ToString() => Value;
+
+        #region Equality Members
         public bool Equals( ExtraDataKey? other )
         {
-            return other != null && other.Value == Value;
+            if( ReferenceEquals( null, other ) )
+            {
+                return false;
+            }
+
+            if( ReferenceEquals( this, other ) )
+            {
+                return true;
+            }
+
+            return Value == other.Value;
         }
 
-        public override string ToString() => Value;
+        public override bool Equals( object? obj )
+        {
+            if( ReferenceEquals( null, obj ) )
+            {
+                return false;
+            }
+
+            if( ReferenceEquals( this, obj ) )
+            {
+                return true;
+            }
+
+            if( obj.GetType() != this.GetType() )
+            {
+                return false;
+            }
+
+            return Equals( (ExtraDataKey)obj );
+        }
+
+        public override int GetHashCode() => Value.GetHashCode();
+        #endregion
     }
 }
