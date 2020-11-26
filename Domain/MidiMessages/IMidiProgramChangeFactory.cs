ab5bdd95dff8b7a146e8c04976275cde79100a98
Hiroaki@R-Koubou <dev.r.koubou@gmail.com>
2020-11-27 02:25:23 +0900

    MIDI pc Factory のチャンネル指定不要の簡易メソッドを削除

--- Domain/MidiMessages/IMidiProgramChangeFactory.cs
+++ Domain/MidiMessages/IMidiProgramChangeFactory.cs
@@ -5,7 +5,6 @@ namespace KeySwitchManager.Domain.MidiMessages
 {
     public interface IMidiProgramChangeFactory : IMidiMessageFactory
     {
-        public MidiProgramChange Create( int pcNumber );
         public MidiProgramChange Create( int channel, int pcNumber );
 
         public static IMidiProgramChangeFactory Default => new DefaultFactory();
@@ -15,11 +14,6 @@ namespace KeySwitchManager.Domain.MidiMessages
             public IMidiMessage Create( int status, int channel, int data1, int data2 )
             {
                 return Create( channel, data1 );
-            }
-
-            public MidiProgramChange Create( int pcNumber )
-            {
-                return Create( 0x00, pcNumber );
             }
 
             public MidiProgramChange Create( int channel, int pcNumber )
