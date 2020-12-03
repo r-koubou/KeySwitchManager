92372c5a32028517fa70e2cb3b3b7f0b459e605e
Hiroaki@R-Koubou <dev.r.koubou@gmail.com>
2020-12-03 17:09:19 +0900

    アセンブリ埋め込みのリソースアクセス追加

--- Common/IO/StreamHelper.cs
+++ Common/IO/StreamHelper.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Reflection;
 
 namespace KeySwitchManager.Common.IO
 {
@@ -6,6 +7,8 @@ namespace KeySwitchManager.Common.IO
     {
         private const int WorkSize = 8192;
         private static readonly byte[] WorkBuffer = new byte[ WorkSize ];
+
+        #region Read, Write
 
         public static void ReadAllAndWrite( Stream source, Stream dest )
         {
@@ -40,6 +43,43 @@ namespace KeySwitchManager.Common.IO
                 rest -= readByte;
             }
         }
+
+        #endregion
+
+        #region Manifest Resource
+
+        /// <summary>
+        /// Alias of Assembly.GetManifestResourceStream(Type, String)
+        /// </summary>
+        /// <typeparam name="TClass">Using for TClass's namespace</typeparam>
+        /// <returns></returns>
+        public static Stream? GetAssemblyResourceStream<TClass>( string resourceName )
+            where TClass : class
+        {
+            var type = typeof( TClass );
+            var asm = Assembly.GetAssembly( type );
+            return asm?.GetManifestResourceStream( type, resourceName );
+        }
+
+        public static byte[] GetAssemblyResourceBytes<TClass>( string resourceName )
+            where TClass : class
+        {
+            var type = typeof( TClass );
+            var asm = Assembly.GetAssembly( type );
+            using var stream = asm?.GetManifestResourceStream( type, resourceName );
+
+            if( stream == null )
+            {
+                return new byte[ 0 ];
+            }
+
+            using var memoryStream = new MemoryStream();
+            ReadAllAndWrite( stream, memoryStream );
+
+            return memoryStream.ToArray();
+        }
+
+        #endregion
 
     }
 }
