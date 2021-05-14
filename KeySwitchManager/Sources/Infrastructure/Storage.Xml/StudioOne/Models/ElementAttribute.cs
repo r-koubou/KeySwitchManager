57d0fdb7146037467c46d8655c9081a1d09c5d2b
Hiroaki@R-Koubou <dev.r.koubou@gmail.com>
2021-05-15 03:50:22 +0900

    StudioOne5.2で追加されたSound Variationのファイルフォーマットに対応

--- KeySwitchManager/Sources/Infrastructure/Storage.Xml/StudioOne/Models/ElementAttribute.cs
+++ KeySwitchManager/Sources/Infrastructure/Storage.Xml/StudioOne/Models/ElementAttribute.cs
@@ -4,10 +4,41 @@ namespace KeySwitchManager.Infrastructure.Storage.Xml.StudioOne.Models
 {
     public class ElementAttribute
     {
-        [XmlAttribute( AttributeName = "pitch" )]
-        public string Pitch { get; set; } = string.Empty;
-
         [XmlAttribute( AttributeName = "name" )]
         public string Name { get; set; } = string.Empty;
+
+        [XmlAttribute( AttributeName = "id" )]
+        public int Id { get; set; }
+
+        [XmlAttribute( AttributeName = "color" )]
+        public string Color { get; set; } = string.Empty;
+
+        [XmlAttribute( AttributeName = "pitch" )]
+        public int Pitch { get; set; }
+
+        [XmlAttribute( AttributeName = "momentary" )]
+        public int Momentary { get; set; }
+
+        [XmlAttribute( AttributeName = "activation" ) ]
+        public string Activation { get; set; } = string.Empty;
+
+        public ElementAttribute()
+        {}
+
+        public ElementAttribute(
+            string name,
+            int id,
+            string color,
+            int pitch,
+            int momentary,
+            string activation )
+        {
+            Name       = name;
+            Id         = id;
+            Color      = color;
+            Pitch      = pitch;
+            Momentary  = momentary;
+            Activation = activation;
+        }
     }
 }
--- KeySwitchManager/Sources/Infrastructure/Storage.Xml/StudioOne/Translators/Helpers/KeySwitchToStudioOneModelHelper.cs
+++ KeySwitchManager/Sources/Infrastructure/Storage.Xml/StudioOne/Translators/Helpers/KeySwitchToStudioOneModelHelper.cs
@@ -1,6 +1,10 @@
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 using KeySwitchManager.Domain.KeySwitches.Models;
+using KeySwitchManager.Domain.KeySwitches.Models.Entities;
+using KeySwitchManager.Domain.KeySwitches.Models.Values;
 using KeySwitchManager.Infrastructure.Storage.Xml.StudioOne.Models;
 
 namespace KeySwitchManager.Infrastructure.Storage.Xml.StudioOne.Translators.Helpers
@@ -14,6 +18,8 @@ namespace KeySwitchManager.Infrastructure.Storage.Xml.StudioOne.Translators.Help
                 Name = $"{source.ProductName.Value} {source.InstrumentName.Value}"
             };
 
+            var id = 0;
+
             foreach( var i in source.Articulations )
             {
                 if( !i.MidiNoteOns.Any() )
@@ -21,15 +27,109 @@ namespace KeySwitchManager.Infrastructure.Storage.Xml.StudioOne.Translators.Help
                     continue;
                 }
 
-                var attr = new ElementAttribute
-                {
-                    Name  = i.ArticulationName.Value,
-                    Pitch = i.MidiNoteOns.First().DataByte1.ToString()!
-                };
+                var attr = TranslateElementAttribute( i, id );
+                id++;
+
                 rootElement.Attributes.Add( attr );
             }
 
             return rootElement;
         }
+
+        #region Translate Attribute
+        private static ElementAttribute TranslateElementAttribute( Articulation articulation, int id )
+        {
+            var name = articulation.ArticulationName.Value;
+            var pitch = articulation.MidiNoteOns[ 0 ].DataByte1.Value;
+            var activation = TranslateActivation( articulation );
+
+            var color = articulation.ExtraData.GetValueOrDefault(
+                ExtraDataKeys.Color, new ExtraDataValue( "FFFF0000" )
+            );
+            var momentary = articulation.ExtraData.GetValueOrDefault(
+                ExtraDataKeys.Momentary, new ExtraDataValue( "0" )
+            ).Value == "0" ? 0 : 1;
+
+            return new ElementAttribute( name, id, color.Value, pitch, momentary, activation );
+        }
+
+        #region Translate Activations
+        private static string TranslateActivation( Articulation articulation )
+        {
+            var activations = new List<string>();
+            var sb = new StringBuilder( 128 );
+
+            activations.AddRange(  TranslateActivationNote( articulation ) );
+            activations.AddRange( TranslateActivationControlChange( articulation ) );
+            activations.AddRange( TranslateActivationProgramChange( articulation ) );
+
+            var count = activations.Count;
+            for( var i = 0; i < count; i++ )
+            {
+                var x = activations[ i ];
+                sb.Append( x );
+
+                if( i < count - 1 )
+                {
+                    sb.Append( '|' );
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        private static IReadOnlyList<string> TranslateActivationNote( Articulation articulation )
+        {
+            var result = new List<string>();
+
+            foreach( var x in articulation.MidiNoteOns )
+            {
+                var byte1 = x.DataByte1.Value;
+                var byte2 = x.DataByte2.Value;
+                result.Add( $"note{byte1}.{byte2}" );
+            }
+
+            return result;
+        }
+
+        private static IReadOnlyCollection<string> TranslateActivationControlChange( Articulation articulation )
+        {
+            var result = new List<string>();
+
+            foreach( var x in articulation.MidiNoteOns )
+            {
+                var ccNo = x.Status.Value;
+                var byte1 = x.DataByte1.Value;
+                var byte2 = x.DataByte2.Value;
+
+                if( ccNo is 0 or 32 )
+                {
+                    result.Add($"bc{byte1}.{byte2}" );
+                }
+                else
+                {
+                    result.Add( $"cc{byte1}.{byte2}" );
+                }
+
+            }
+
+            return result;
+        }
+
+        private static IReadOnlyCollection<string> TranslateActivationProgramChange( Articulation articulation )
+        {
+            var result = new List<string>();
+
+            foreach( var x in articulation.MidiProgramChanges )
+            {
+                var byte1 = x.DataByte1.Value;
+                result.Add( $"pc{byte1}" );
+            }
+
+            return result;
+        }
+        #endregion
+        #endregion
+
     }
 }
