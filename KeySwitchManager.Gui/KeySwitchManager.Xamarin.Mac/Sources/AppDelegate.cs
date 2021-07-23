dd3086bbd9407032753f345ae5de3956e1d3826d
Hiroaki@R-Koubou <dev.r.koubou@gmail.com>
2021-07-24 07:04:29 +0900

    WIP: GUI試作

---
+++ KeySwitchManager.Gui/KeySwitchManager.Xamarin.Mac/Sources/AppDelegate.cs
@@ -0,0 +1,25 @@
+﻿using AppKit;
+
+using Foundation;
+
+namespace KeySwitchManager.Xamarin.Mac
+{
+    [Register( "AppDelegate" )]
+    public class AppDelegate : NSApplicationDelegate
+    {
+        public AppDelegate()
+        {
+            /*caret*/
+        }
+
+        public override void DidFinishLaunching( NSNotification notification )
+        {
+            // Insert code here to initialize your application
+        }
+
+        public override void WillTerminate( NSNotification notification )
+        {
+            // Insert code here to tear down your application
+        }
+    }
+}
---
+++ KeySwitchManager.Gui/KeySwitchManager.Xamarin.Mac/Sources/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace KeySwitchManager.Xamarin.Mac
+{
+    public static class MainClass
+    {
+        public static void Main( string[] args )
+        {
+            NSApplication.Init();
+            NSApplication.Main( args );
+        }
+    }
+}
