8a3df36ac3f28d37d63107d814237ea27071e881
Hiroaki@R-Koubou <dev.r.koubou@gmail.com>
2021-07-25 16:42:51 +0900

    WIP: GUI試作

--- KeySwitchManager.Gui/KeySwitchManager.Xamarin.Mac/Sources/ViewController.cs
+++ KeySwitchManager.Gui/KeySwitchManager.Xamarin.Mac/Sources/ViewController.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-
 using AppKit;
-
 using Foundation;
 
 namespace KeySwitchManager.Xamarin.Mac
@@ -10,6 +8,7 @@ namespace KeySwitchManager.Xamarin.Mac
     {
         public ViewController( IntPtr handle ) : base( handle ) {}
 
+        #region Application LifeCycle
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
@@ -26,5 +25,57 @@ namespace KeySwitchManager.Xamarin.Mac
                 // Update the view, if already loaded.
             }
         }
+        #endregion
+
+        #region UI Event Handlers
+        partial void OnOpenNewFileButtonClicked( Foundation.NSObject sender )
+        {
+            var selectedFile = ChooseSaveFilePath( "yaml", "xlsx" );
+        }
+        #endregion
+
+        #region Utilities
+        private string ChooseOpenFilePath( params string[] extensions )
+        {
+            var result = string.Empty;
+
+            var dialog = new NSOpenPanel()
+            {
+                CanChooseDirectories = false,
+                AllowsMultipleSelection = false,
+                AllowedFileTypes = extensions
+            };
+
+            dialog.Begin( ( num ) => {
+                if( num == (long)NSModalResponse.OK )
+                {
+                    result = dialog.Filenames[ 0 ];
+                }
+            });
+
+            return result;
+        }
+
+        private string ChooseSaveFilePath( params string[] extensions )
+        {
+            var result = string.Empty;
+
+            var dialog = new NSSavePanel()
+            {
+                CanCreateDirectories    = true,
+                AllowsOtherFileTypes    = false,
+                AllowedFileTypes        = extensions
+            };
+
+            dialog.Begin( ( num ) => {
+                if( num == (long)NSModalResponse.OK )
+                {
+                    result = dialog.Filename;
+                }
+            });
+
+            return result;
+        }
+        #endregion
     }
 }
--- KeySwitchManager.Gui/KeySwitchManager.Xamarin.Mac/Sources/ViewController.designer.cs
+++ KeySwitchManager.Gui/KeySwitchManager.Xamarin.Mac/Sources/ViewController.designer.cs
@@ -1,19 +1,39 @@
 ﻿// WARNING
 //
-// This file has been generated automatically by Xamarin Studio to store outlets and
-// actions made in the UI designer. If it is removed, they will be lost.
+// This file has been generated automatically by Rider IDE
+//   to store outlets and actions made in Xcode.
+// If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
-
 using Foundation;
+using System.CodeDom.Compiler;
 
 namespace KeySwitchManager.Xamarin.Mac
 {
-    [Register("ViewController")]
-    partial class ViewController
-    {
-        void ReleaseDesignerOutlets()
-        {
-        }
-    }
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		AppKit.NSTextField NewFileTextField { get; set; }
+
+		[Outlet]
+		AppKit.NSButton OpenNewFileButton { get; set; }
+
+		[Action ("OnOpenNewFileButtonClicked:")]
+		partial void OnOpenNewFileButtonClicked (Foundation.NSObject sender);
+
+		void ReleaseDesignerOutlets ()
+		{
+			if (NewFileTextField != null) {
+				NewFileTextField.Dispose ();
+				NewFileTextField = null;
+			}
+
+			if (OpenNewFileButton != null) {
+				OpenNewFileButton.Dispose ();
+				OpenNewFileButton = null;
+			}
+
+		}
+	}
 }
