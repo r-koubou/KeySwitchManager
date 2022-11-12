8ef9b26a864c5ea84ae00879fdc4bead238164b3
Hiroaki@R-Koubou <dev.r.koubou@gmail.com>
2022-11-13 04:12:36 +0900

    DateTime対応

---
+++ KeySwitchManager/Sources/Runtime/Commons/Data/Extensions/UtcDateTimeExtension.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace KeySwitchManager.Commons.Data.Extensions
+{
+    public static class UtcDateTimeExtension
+    {
+        public static DateTime As(this UtcDateTime utcDateTime)
+        {
+            return new DateTime(
+                year: utcDateTime.Year,
+                month: utcDateTime.Month,
+                day:utcDateTime.Day,
+                hour: utcDateTime.Hour,
+                minute: utcDateTime.Minute,
+                second: utcDateTime.Second,
+                millisecond: utcDateTime.MilliSecond
+            );
+        }
+    }
+}
