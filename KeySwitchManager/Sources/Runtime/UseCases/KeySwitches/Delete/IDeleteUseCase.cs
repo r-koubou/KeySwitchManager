fae249647e755ba124ee7f88e9be6611ad2631d3
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2023-10-21 23:41:10 +0900

    usecaseの非同期メソッド対応

--- KeySwitchManager/Sources/Runtime/UseCases/KeySwitches/Delete/IDeleteUseCase.cs
+++ KeySwitchManager/Sources/Runtime/UseCases/KeySwitches/Delete/IDeleteUseCase.cs
@@ -1,7 +1,12 @@
+using System.Threading.Tasks;
+
 namespace KeySwitchManager.UseCase.KeySwitches.Delete
 {
     public interface IDeleteUseCase
     {
-        public DeleteResponse Execute( DeleteRequest request );
+        public DeleteResponse Execute( DeleteRequest request )
+            => ExecuteAsync( request ).GetAwaiter().GetResult();
+
+        public Task<DeleteResponse> ExecuteAsync( DeleteRequest request );
     }
 }
--- KeySwitchManager/Sources/Runtime/UseCases/KeySwitches/Dump/IDumpFileUseCase.cs
+++ KeySwitchManager/Sources/Runtime/UseCases/KeySwitches/Dump/IDumpFileUseCase.cs
@@ -1,7 +1,12 @@
+using System.Threading.Tasks;
+
 namespace KeySwitchManager.UseCase.KeySwitches.Dump
 {
     public interface IDumpFileUseCase
     {
-        public DumpFileResponse Execute( DumpFileRequest request );
+        public DumpFileResponse Execute( DumpFileRequest request )
+            => ExecuteAsync( request ).GetAwaiter().GetResult();
+
+        public Task<DumpFileResponse> ExecuteAsync( DumpFileRequest request );
     }
 }
