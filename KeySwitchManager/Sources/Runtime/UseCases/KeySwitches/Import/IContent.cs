be40374ec416e8376c382b6a97027bade1c801b8
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2023-10-31 21:20:26 +0900

    不要となったメモを削除

--- KeySwitchManager/Sources/Runtime/UseCases/KeySwitches/Import/IContent.cs
+++ KeySwitchManager/Sources/Runtime/UseCases/KeySwitches/Import/IContent.cs
@@ -6,11 +6,6 @@ namespace KeySwitchManager.UseCase.KeySwitches.Import
 {
     public interface IContent
     {
-        // メモ：データのソース元ストリームを実装する
-        // 例：
-        // ファイル：FileStream を返す
-        // HTTP：レスポンスのストリームを返す
-
         Stream GetContentStream()
             => GetContentStreamAsync().GetAwaiter().GetResult();
 
--- KeySwitchManager/Sources/Runtime/UseCases/KeySwitches/Import/IImportContentReader.cs
+++ KeySwitchManager/Sources/Runtime/UseCases/KeySwitches/Import/IImportContentReader.cs
@@ -8,7 +8,6 @@ namespace KeySwitchManager.UseCase.KeySwitches.Import
 {
     public interface IImportContentReader
     {
-        // メモ：content から得られるストリームを元にファイルタイプ毎の読み込みの具体を実装する
         Task<IReadOnlyCollection<KeySwitch>> ReadAsync( IContent content, CancellationToken cancellationToken = default );
     }
 }
