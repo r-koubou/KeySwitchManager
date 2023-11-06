750201b286fde15b779408b8b586c02683146495
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2023-11-06 20:26:34 +0900

    kindの明示的にUTCを指定し忘れていたので対応

--- KeySwitchManager/Sources/Runtime/Commons/Data/Extensions/UtcDateTimeExtension.cs
+++ KeySwitchManager/Sources/Runtime/Commons/Data/Extensions/UtcDateTimeExtension.cs
@@ -4,12 +4,13 @@ namespace KeySwitchManager.Commons.Data.Extensions
 {
     public static class UtcDateTimeExtension
     {
-        public static DateTime As(this UtcDateTime utcDateTime)
+        public static DateTime As( this UtcDateTime utcDateTime )
         {
             return new DateTime(
+                kind: DateTimeKind.Utc,
                 year: utcDateTime.Year,
                 month: utcDateTime.Month,
-                day:utcDateTime.Day,
+                day: utcDateTime.Day,
                 hour: utcDateTime.Hour,
                 minute: utcDateTime.Minute,
                 second: utcDateTime.Second,
