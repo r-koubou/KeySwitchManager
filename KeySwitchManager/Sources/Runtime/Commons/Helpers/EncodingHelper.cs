fec8ac1f8e03a3c58e939bc64a5ac1cc38d55120
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2023-11-06 20:22:19 +0900

    ITF8N（BOM無し）に変更

---
+++ KeySwitchManager/Sources/Runtime/Commons/Helpers/EncodingHelper.cs
@@ -0,0 +1,14 @@
+using System.Text;
+
+namespace KeySwitchManager.Commons.Helpers
+{
+    public static class EncodingHelper
+    {
+        // ReSharper disable InconsistentNaming
+        public static Encoding UTF8 => Encoding.UTF8;
+        public static Encoding UTF8N { get; } = new UTF8Encoding( false );
+        // ReSharper restore InconsistentNaming
+
+        public static Encoding Default => UTF8N;
+    }
+}
