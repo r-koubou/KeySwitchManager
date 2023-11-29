94c6b4dc7a733b000184307853a48480aeca0202
Hiroaki@R-Koubou <dev.r.koubou@gmail.com>
2023-11-30 01:16:54 +0900

    別ファイルに独立させるのをし忘れていたので対応

--- KeySwitchManager/Sources/Runtime/UseCases/KeySwitches/Export/ExportOutputData.cs
+++ KeySwitchManager/Sources/Runtime/UseCases/KeySwitches/Export/ExportOutputData.cs
@@ -1,25 +1,9 @@
 using System;
-using System.Collections.Generic;
 
-using KeySwitchManager.Domain.KeySwitches.Models;
 using KeySwitchManager.UseCase.Commons;
 
 namespace KeySwitchManager.UseCase.KeySwitches.Export
 {
-    public class ExportOutputValue
-    {
-        public ExportInputValue Input { get; }
-        public IReadOnlyCollection<KeySwitch> Result { get; }
-        public int WrittenCount { get; }
-
-        public ExportOutputValue( ExportInputValue input, IReadOnlyCollection<KeySwitch> result )
-        {
-            Input        = input;
-            Result       = result;
-            WrittenCount = result.Count;
-        }
-    }
-
     public class ExportOutputData : OutputData<ExportOutputValue>
     {
         public ExportOutputData( bool result, ExportOutputValue value, Exception? error = null ) : base( result, value, error ) {}
